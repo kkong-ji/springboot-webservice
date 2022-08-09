d5f8b87dd17682f3b7d26822ee532341449d13e6
Kong ji han <wisejohn950330@gmail.com>
2022-08-09 16:21:49 +0900

    Mustache로 화면 구성하기

--- src/main/java/springboot/service/posts/PostsService.java
+++ src/main/java/springboot/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import springboot.domain.posts.Posts;
 import springboot.domain.posts.PostsRepository;
+import springboot.web.dto.PostsListResponseDto;
 import springboot.web.dto.PostsResponseDto;
 import springboot.web.dto.PostsSaveRequestDto;
 import springboot.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -27,6 +31,20 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()  -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById (Long id) {
